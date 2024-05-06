6c1a13de3b8f620895c369f0bafe49755d03cfd5
Tzahi Bar <tzahib@jfrog.com>
2024-05-06 21:54:02 +0300

    Java - 11

---
+++ src/test/java/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    @Test
+    @DisplayName("Test the answer to everything")
+    public void test() {
+        assertEquals(42, 42);
+    }
+}
