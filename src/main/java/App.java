ac7856451d55a398d3f3b4c9eea7d90bf9bc2a74
Tzahi Bar <tzahib@jfrog.com>
2024-05-06 22:04:09 +0300

    Java - 13

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("I am your awesome Java application!");
+        System.out.println("Hello " + args[0]);
     }
 }
