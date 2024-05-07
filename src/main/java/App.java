2379883fad97e2cf637c9f2ead46fe0e72c02b7d
Tzahi Bar <tzahib@jfrog.com>
2024-05-07 11:42:46 +0300

    Java - 23

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Number of arguments: " + args.length);
         System.out.println("Hello " + args[0]);
     }
 }
