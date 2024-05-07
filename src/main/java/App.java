0bd85bb7bfb2c2c5bd18134e44264b00edc3750a
Tzahi Bar <tzahib@jfrog.com>
2024-05-07 16:34:58 +0300

    Java Dev - 16

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        String arg1 = System.getProperty("INPUT_ARG1");
-        System.out.println("Hello " + arg1);
+        System.out.println("Hello " + args[0] + "!");
     }
 }
