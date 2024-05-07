fe4f5e8146a5b3bd19e4f333c3a4de94a2400a2f
Tzahi Bar <tzahib@jfrog.com>
2024-05-07 14:36:49 +0300

    Java Dev - 11

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello " + args[0]);
+        String arg1 = System.getProperty("who-to-greet");
+        System.out.println("Hello " + arg1);
     }
 }
